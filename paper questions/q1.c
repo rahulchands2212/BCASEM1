bdccf52259410ae02034ee691fede6d25c778abc
Rahul Chand <chandrahuls2212@gmail.com>
2026-01-02 17:28:19 +0530

    paper code solve

---
+++ paper questions/q1.c
@@ -0,0 +1,20 @@
+/* find area of square */
+
+#include<stdio.h>	//header file.
+
+int main(){	//pre define function.
+
+//declared variable .
+int side;
+
+//print statement ;
+printf("enter side of square : ");
+
+//take input by user
+scanf("%d",&side);
+
+//logic and print output on screen ;
+printf("area of square : %d",side*side);
+
+return 0;
+}
